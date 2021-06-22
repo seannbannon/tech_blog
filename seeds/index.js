19efc231d76929a63ac7ab2f5d4c42de32b10fa4
Sean Bannon <seannbannon@gmail.com>
2021-06-21 20:38:49 -0500

    showing seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,15 +1,31 @@
-const seedUsers = require('./user-seeds');
-const seedPosts = require('./post-seeds');
-const seedComments = require('./comment-seeds');
-
 const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
 
-const seedAll = async() => {
-    await sequelize.sync({ force: true });
-    await seedUsers();
-    await seedPosts();
-    await seedComments();
-    process.exit(0);
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postData) {
+    await Post.create({
+      ...post,
+    });
+  }
+  
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment,
+    });
+  }
+
+  process.exit(0);
 };
 
-seedAll();
+seedDatabase();
